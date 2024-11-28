d6c404ddc463d29dce04eec42083f78e0356b5a8
Hitusss <kuba.niespielak@gmail.com>
2024-11-28 21:52:21 +0100

    add deleted add deleteItem to store

--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,17 @@ import { type ListItem } from "./api/getListData";
 
 type State = {
   expanded: Set<ListItem["id"]>;
+  deleted: Set<ListItem["id"]>;
 };
 
 type Actions = {
   toggleExpanded: (id: ListItem["id"]) => void;
+  deleteItem: (id: ListItem["id"]) => void;
 };
 
 export const useStore = create<State & Actions>((set) => ({
   expanded: new Set(),
+  deleted: new Set(),
   toggleExpanded(id) {
     set((state) => {
       const expanded = new Set(state.expanded);
@@ -20,6 +23,13 @@ export const useStore = create<State & Actions>((set) => ({
         expanded.add(id);
       }
       return { expanded };
+    });
+  },
+  deleteItem(id) {
+    set((state) => {
+      const deleted = new Set(state.deleted);
+      deleted.add(id);
+      return { deleted };
     });
   },
 }));
