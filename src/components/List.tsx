3d639e3a981886e942df9329961de95a677adad7
Hitusss <kuba.niespielak@gmail.com>
2024-11-28 21:56:59 +0100

    add deleted variant to list Card

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -9,31 +9,50 @@ import {
 } from "./Collapsible";
 import { useStore } from "../store";
 
+const variants = {
+  default: "border-black",
+  deleted: "border-rose-500",
+};
+
 type CardProps = {
+  variant?: keyof typeof variants;
   id: ListItem["id"];
   title: ListItem["title"];
   description: ListItem["description"];
 };
 
-export const Card: FC<CardProps> = ({ id, title, description }) => {
+export const Card: FC<CardProps> = ({
+  variant = "default",
+  id,
+  title,
+  description,
+}) => {
   const open = useStore((state) => state.expanded.has(id));
   const toggleExpanded = useStore((state) => state.toggleExpanded);
   const deleteItem = useStore((state) => state.deleteItem);
   return (
     <Collapsible open={open} onOpenChange={() => toggleExpanded(id)}>
-      <div className="border border-black px-2 py-1.5">
+      <div className={`border px-2 py-1.5 ${variants[variant]}`}>
         <div className="flex justify-between mb-0.5">
           <h1 className="font-medium">{title}</h1>
-          <div className="flex">
-            <CollapsibleButton />
-            <Button variant="ghost" size="icon" onClick={() => deleteItem(id)}>
-              <XMarkIcon />
-            </Button>
-          </div>
+          {variant === "default" && (
+            <div className="flex">
+              <CollapsibleButton />
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => deleteItem(id)}
+              >
+                <XMarkIcon />
+              </Button>
+            </div>
+          )}
         </div>
-        <CollapsibleContent>
-          <p className="text-sm">{description}</p>
-        </CollapsibleContent>
+        {variant === "default" && (
+          <CollapsibleContent>
+            <p className="text-sm">{description}</p>
+          </CollapsibleContent>
+        )}
       </div>
     </Collapsible>
   );
