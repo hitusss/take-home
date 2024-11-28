e9731cbeaca7ad639323144779efdff5d5c4b868
Hitusss <kuba.niespielak@gmail.com>
2024-11-28 19:11:03 +0100

    replace html button with custom Button component in Entrypoint

--- src/components/Entrypoint.tsx
+++ src/components/Entrypoint.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { ListItem, useGetListData } from "../api/getListData";
 import { Card } from "./List";
 import { Spinner } from "./Spinner";
+import { Button } from "./Button";
 
 export const Entrypoint = () => {
   const [visibleCards, setVisibleCards] = useState<ListItem[]>([]);
@@ -25,22 +26,23 @@ export const Entrypoint = () => {
   return (
     <div className="flex gap-x-16">
       <div className="w-full max-w-xl">
-        <h1 className="mb-1 font-medium text-lg">My Awesome List ({visibleCards.length})</h1>
+        <h1 className="mb-1 font-medium text-lg">
+          My Awesome List ({visibleCards.length})
+        </h1>
         <div className="flex flex-col gap-y-3">
           {visibleCards.map((card) => (
-            <Card key={card.id} title={card.title} description={card.description} />
+            <Card
+              key={card.id}
+              title={card.title}
+              description={card.description}
+            />
           ))}
         </div>
       </div>
       <div className="w-full max-w-xl">
         <div className="flex items-center justify-between">
           <h1 className="mb-1 font-medium text-lg">Deleted Cards (0)</h1>
-          <button
-            disabled
-            className="text-white text-sm transition-colors hover:bg-gray-800 disabled:bg-black/75 bg-black rounded px-3 py-1"
-          >
-            Reveal
-          </button>
+          <Button disabled>Reveal</Button>
         </div>
         <div className="flex flex-col gap-y-3">
           {/* {deletedCards.map((card) => (
