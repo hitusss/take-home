08ca957008605e8b7affb6f03cd3186fd996ea48
Hitusss <kuba.niespielak@gmail.com>
2024-11-28 21:20:30 +0100

    add expanded and toggleExpanded to store

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,25 @@
 import { create } from "zustand";
+import { type ListItem } from "./api/getListData";
 
-type State = {};
+type State = {
+  expanded: Set<ListItem["id"]>;
+};
 
-type Actions = {};
+type Actions = {
+  toggleExpanded: (id: ListItem["id"]) => void;
+};
 
-export const useStore = create<State & Actions>((set) => ({}));
+export const useStore = create<State & Actions>((set) => ({
+  expanded: new Set(),
+  toggleExpanded(id) {
+    set((state) => {
+      const expanded = new Set(state.expanded);
+      if (expanded.has(id)) {
+        expanded.delete(id);
+      } else {
+        expanded.add(id);
+      }
+      return { expanded };
+    });
+  },
+}));
